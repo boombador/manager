c8f381480087eb1ebed7e15ec3722e43434b6c40
Ian McLaughlin <ian.p.mclaughlin@gmail.com>
2014-03-16 21:15:46 -0400

    Separate logic of parameters and setup a little

--- manage.sh
+++ manage.sh
@@ -1,25 +1,37 @@
 #!/usr/bin/env bash
-# must be called as `. manage.sh`, probably good to alias
-
-# this script will detect the passed project name and set up the environment for working on it
-# this script should set up tmux sessions, windows, panes
-# separate driver script attaches you to your tmux session for the project
-# should store this project setup script in ~/.manager/
 
 # ~/.manager/
 # ~/.manager/system/* " setup options specific to hostname
 # ~/.manager/projects/* " project specific aliases, tmux setup, env variables
 
-# simple system to get started
+# no parameter
+    # list active project
+
 PROJECT=$1
+
+# kind of a hacky check
+if $(ps -e | grep -q tmux) ; then
+    CURPROJECT=`tmux display-message -p "#S"`
+fi
+
+if [ -z $PROJECT ] ; then
+    if [ -n $CURPROJECT ] ; then
+        echo "Current project:" $CURPROJECT
+        exit 0
+    else
+        echo "Manager not active"
+        exit 0
+    fi
+fi
+
 case $PROJECT in
-    "manager" )
-        PROJDIR=~/dev/manager
-        cd $PROJDIR
-        echo "Beginning work on " $PROJECT
-        echo "Entering " $PROJDIR
-        tmux new -s $PROJECT
-        tmux split-window -v
-        tmux new-window -t $PROJECT:1 -n 'vim'
+    "manager" ) PROJDIR=~/dev/manager ;; 
+    * ) exit 0 ;;
 esac
 
+echo "setting up tmux..."
+
+tmux new-session -d -s $PROJECT -c $PROJDIR
+tmux send-keys "vim" C-m
+tmux split-window -v
+tmux attach-session -t $PROJECT
